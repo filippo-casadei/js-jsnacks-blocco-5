da949c3c44969d1fb9165149afb21bbd0f098251
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-14 16:48:11 +0200

    Risolta task 1.

--- ex-js-array-methods/snack-1/script.js
+++ ex-js-array-methods/snack-1/script.js
@@ -2,3 +2,9 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// utilizzo forEach per scorrere l'array ed eseguire la funzione per ogni elemento name
+
+names.forEach(function (name) {
+    console.log(name)
+});
