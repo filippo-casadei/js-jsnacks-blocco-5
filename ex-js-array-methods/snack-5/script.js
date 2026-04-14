7f17ec8d4dfe7a05f37584d425820fe5d7423dad
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-14 17:30:11 +0200

    inserito ragionamento mentale commentato per la task 5.

--- ex-js-array-methods/snack-5/script.js
+++ ex-js-array-methods/snack-5/script.js
@@ -2,3 +2,10 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// qui userò filter perchè non devo modificare nulla.
+// i numeri dovranno essere quelli % 2 === 0
+// vuole un return che è sempre true o false
+
+
+
