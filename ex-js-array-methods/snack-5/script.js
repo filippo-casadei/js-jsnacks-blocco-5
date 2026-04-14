5607d7a48102699a570d43ee8a9ad7e18f1a7e94
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-14 17:51:31 +0200

    Risolto task 5

--- ex-js-array-methods/snack-5/script.js
+++ ex-js-array-methods/snack-5/script.js
@@ -7,5 +7,19 @@ const nums = [2, 8, 4, 7, 12, 87];
 // i numeri dovranno essere quelli % 2 === 0
 // vuole un return che è sempre true o false
 
+ let arrayCheContieneSolamenteNumeriPari = nums.filter (function (number) {
+    if (number % 2 === 0) 
+        return true
+});
+
+
+
+/* // potrei scriverlo anche così ma mi confonde veramente le idee:
+let arrayCheContieneSolamenteNumeriPari = nums.filter (function (number) {
+return number % 2 === 0 
+}); */
+
+console.log(arrayCheContieneSolamenteNumeriPari);
+
 
 
