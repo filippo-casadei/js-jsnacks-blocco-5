24a5295945f639f8b528ed67d745942246c67746
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-14 17:10:27 +0200

    Risolta task 3.

--- ex-js-array-methods/snack-3/script.js
+++ ex-js-array-methods/snack-3/script.js
@@ -2,3 +2,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// qui userò .map e chiederò un return perchè per .map è obbligatorio
+// al return aggiungo +1
+
+let nuovoArray = numbers.map (function(numero) {
+    return (numero +1)
+});
+
+console.log(nuovoArray);
